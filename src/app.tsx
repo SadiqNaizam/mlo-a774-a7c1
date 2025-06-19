56def6d1d75a2424cce65484512dba85fd11ef19
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-19 23:09:20 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,38 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+
+const App = () => (
+<QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+        <Routes>
+
+
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          {/* catch-all */}
+          <Route path="*" element={<NotFound />} />
+
+
+        </Routes>
+    </BrowserRouter>
+    </TooltipProvider>
+</QueryClientProvider>
+);
+
+export default App;
